a0f98f5ede1fc29abd0009419ea3eb9e7205a24e
Gian Marco <gianmarcomansueti@live.it>
2025-03-07 17:41:46 +0100

    Aggiunto build.sh e aggiornato Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: gunicorn my_django_app.wsgi
+web: ./build.sh && gunicorn my_django_app.wsgi
+
---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt-get update
+apt-get install -y build-essential
+apt-get install -y python3-dev
+apt-get install -y libatlas-base-dev
+apt-get install -y gfortran
